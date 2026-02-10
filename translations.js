c3e8e720cedf14341600837235d292a2ec1ea4fa
graphineer <shalumovboris@yahoo.com>
2026-02-10 23:47:07 +0100

    Fix focus padding handling

--- translations.js
+++ translations.js
@@ -269,7 +269,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       }
     }
     if(disciplines){
-      disciplines.style.paddingBottom = targetLang === 'ru' ? '5rem' : '0';
+      disciplines.style.paddingBottom = targetLang === 'ru' ? '5rem' : '';
     }
   }
 
